fa44a7fd8cfd6f2800de62b0b8d7a2df883d6006
Aldi Kabulov <reservecrate@gmail.com>
2022-07-31 17:04:51 +0600

    formatted src/lib/index.tsx with Prettier

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import { useState, useEffect } from 'react';
 
 export type imgResolution =
-  | "default"
-  | "mqdefault"
-  | "hqdefault"
-  | "sddefault"
-  | "maxresdefault";
+  | 'default'
+  | 'mqdefault'
+  | 'hqdefault'
+  | 'sddefault'
+  | 'maxresdefault';
 
 interface LiteYouTube {
   announce?: string;
@@ -25,46 +25,51 @@ interface LiteYouTube {
   poster?: imgResolution;
   webp?: boolean;
   wrapperClass?: string;
-  onIframeAdded?: () => void
-  muted?: boolean,
-  thumbnail?: string,
-  height?: number|string,
-  width?: number|string,
+  onIframeAdded?: () => void;
+  muted?: boolean;
+  thumbnail?: string;
+  height?: number | string;
+  width?: number | string;
 }
 
 export default function LiteYouTubeEmbed(props: LiteYouTube) {
-  const [preconnected] = React.useState(false);
-  const [iframe] = React.useState(false);
+  const [preconnected] = useState(false);
+  const [iframe] = useState(false);
   const videoId = encodeURIComponent(props.id);
-  const videoPlaylisCovertId = typeof props.playlistCoverId === 'string' ? encodeURIComponent(props.playlistCoverId) : null;
+  const videoPlaylisCovertId =
+    typeof props.playlistCoverId === 'string'
+      ? encodeURIComponent(props.playlistCoverId)
+      : null;
   const videoTitle = props.title;
-  const posterImp = props.poster || "hqdefault";
-  const paramsImp = `&${props.params}` || "";
-  const mutedImp = props.muted ? "&mute=1" : "";
+  const posterImp = props.poster || 'hqdefault';
+  const paramsImp = `&${props.params}` || '';
+  const mutedImp = props.muted ? '&mute=1' : '';
   const format = props.webp ? 'webp' : 'jpg';
-  const vi = props.webp ? 'vi_webp' : 'vi'
-  const posterUrl = props.thumbnail || (!props.playlist 
-    ? `https://i.ytimg.com/${vi}/${videoId}/${posterImp}.${format}` 
-    : `https://i.ytimg.com/${vi}/${videoPlaylisCovertId}/${posterImp}.${format}`);
+  const vi = props.webp ? 'vi_webp' : 'vi';
+  const posterUrl =
+    props.thumbnail ||
+    (!props.playlist
+      ? `https://i.ytimg.com/${vi}/${videoId}/${posterImp}.${format}`
+      : `https://i.ytimg.com/${vi}/${videoPlaylisCovertId}/${posterImp}.${format}`);
 
   let ytUrl = props.noCookie
-    ? "https://www.youtube-nocookie.com"
-    : "https://www.youtube.com";
+    ? 'https://www.youtube-nocookie.com'
+    : 'https://www.youtube.com';
   ytUrl = props.cookie
-    ? "https://www.youtube.com"
-    : "https://www.youtube-nocookie.com";
+    ? 'https://www.youtube.com'
+    : 'https://www.youtube-nocookie.com';
 
   const iframeSrc = !props.playlist
     ? `${ytUrl}/embed/${videoId}?state=1${mutedImp}${paramsImp}`
     : `${ytUrl}/embed/videoseries?${mutedImp}&list=${videoId}${paramsImp}`;
 
   const adNetworkImp = props.adNetwork || false;
-  const iframeClassImp = props.iframeClass || "";
-  const onIframeAdded = props.onIframeAdded || function () { };
-  const height = props.height || "560";
-  const width = props.width || "315";
+  const iframeClassImp = props.iframeClass || '';
+  const onIframeAdded = props.onIframeAdded || function () {};
+  const height = props.height || '560';
+  const width = props.width || '315';
 
-/*
+  /*
   const warmConnections = () => {
     if (preconnected) return;
     setPreconnected(true);
@@ -74,8 +79,9 @@ export default function LiteYouTubeEmbed(props: LiteYouTube) {
     if (iframe) return;
     setIframe(true);
   };
-*/
-  React.useEffect(() => {
+  */
+
+  useEffect(() => {
     if (iframe) {
       onIframeAdded();
     }
@@ -83,22 +89,18 @@ export default function LiteYouTubeEmbed(props: LiteYouTube) {
 
   return (
     <>
-      <link
-        rel="preload"
-        href={posterUrl}
-        as="image"
-      />
+      <link rel='preload' href={posterUrl} as='image' />
       <>
         {preconnected && (
           <>
-            <link rel="preconnect" href={ytUrl} />
-            <link rel="preconnect" href="https://www.google.com" />
+            <link rel='preconnect' href={ytUrl} />
+            <link rel='preconnect' href='https://www.google.com' />
             {adNetworkImp && (
               <>
-                <link rel="preconnect" href="https://static.doubleclick.net" />
+                <link rel='preconnect' href='https://static.doubleclick.net' />
                 <link
-                  rel="preconnect"
-                  href="https://googleads.g.doubleclick.net"
+                  rel='preconnect'
+                  href='https://googleads.g.doubleclick.net'
                 />
               </>
             )}
@@ -110,12 +112,11 @@ export default function LiteYouTubeEmbed(props: LiteYouTube) {
         title={videoTitle}
         width={width}
         height={height}
-        frameBorder="0"
-        allow="encrypted-media; picture-in-picture"
+        frameBorder='0'
+        allow='encrypted-media; picture-in-picture'
         allowFullScreen
         src={iframeSrc}
-      ></iframe>
+      />
     </>
   );
 }
-
